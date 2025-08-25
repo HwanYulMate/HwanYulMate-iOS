9414e16eecf138dbb9735f6ced7365d9b390512d
KJH <gh7052@gmail.com>
2025-08-25 18:49:55 +0900

    [Feat] #3 - 탭 바 구현

--- HwanYulMate/Application/SceneDelegate.swift
+++ HwanYulMate/Application/SceneDelegate.swift
@@ -13,13 +13,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
+        configureNavigationBarAppearence()
+        
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = HomeViewController()
+        window?.rootViewController = TabBarViewController()
         window?.makeKeyAndVisible()
     }
 
@@ -50,7 +49,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
+    
+    private func configureNavigationBarAppearence() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.shadowImage = nil
+        appearance.shadowColor = nil
+        appearance.backgroundColor = .white
+//        appearance.titleTextAttributes = [.foregroundColor: UIColor.primaryText]
+//        UINavigationBar.appearance().tintColor
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
 }
-
---
+++ HwanYulMate/Presentation/TabBar/TabBarViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TabBarViewController.swift
+//  HwanYulMate
+//
+//  Created by 김정호 on 8/25/25.
+//
+
+import UIKit
+
+final class TabBarViewController: UITabBarController {
+    
+    // MARK: - life cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureTabBarController()
+        configureTabBarAppearence()
+    }
+    
+    // MARK: - methods
+    private func configureTabBarController() {
+        let homeVC = UINavigationController(rootViewController: HomeViewController())
+        homeVC.tabBarItem = UITabBarItem(title: "환율", image: .exchangeRate, tag: 0)
+        
+        setViewControllers([homeVC], animated: true)
+    }
+    
+    private func configureTabBarAppearence() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = .white
+        appearance.stackedLayoutAppearance.normal.iconColor = .gray400
+        appearance.stackedLayoutAppearance.selected.iconColor = .green500
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.gray400]
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.green500]
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+    }
+}
