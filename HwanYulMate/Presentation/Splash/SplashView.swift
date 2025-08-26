55cab87f9958b116b58619e7e1230571045edbd4
KJH <gh7052@gmail.com>
2025-08-26 18:22:23 +0900

    [Feat] #5 - LaunchScreen 구현

---
+++ HwanYulMate/Presentation/Splash/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  HwanYulMate
+//
+//  Created by 김정호 on 8/26/25.
+//
+
+import UIKit
+
+class SplashView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
