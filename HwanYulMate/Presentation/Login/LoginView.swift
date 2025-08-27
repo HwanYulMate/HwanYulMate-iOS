cd912e32e21894e1e31c43c6f1a018a6d4236a73
KJH <gh7052@gmail.com>
2025-08-27 20:23:38 +0900

    [Feat] #5 - Login 화면 UI 구현

--- HwanYulMate/Presentation/Login/LoginView.swift
+++ HwanYulMate/Presentation/Login/LoginView.swift
@@ -6,15 +6,84 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class LoginView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+final class LoginView: BaseView {
+    
+    // MARK: - properties
+    private let logoImageView = UIImageView().then {
+        $0.image = .loginLogo
     }
-    */
-
+    
+    private let loginTitleLabel = UILabel().then {
+        $0.text = "환율메이트 로그인"
+        $0.font = .pretendard(size: 14, weight: .medium)
+        $0.textColor = .gray400
+        $0.textAlignment = .center
+    }
+    
+    let googleLoginButton = UIButton()
+    
+    let appleLoginButton = UIButton()
+    
+    // MARK: - methods
+    override func configureUI() {
+        super.configureUI()
+        
+        configureSocialLoginButton(googleLoginButton, title: "Google로 로그인", image: .google)
+        configureSocialLoginButton(appleLoginButton, title: "Apple로 로그인", image: .apple)
+    }
+    
+    override func configureHierarchy() {
+        addSubview(logoImageView)
+        addSubview(loginTitleLabel)
+        addSubview(googleLoginButton)
+        addSubview(appleLoginButton)
+    }
+    
+    override func configureConstraints() {
+        logoImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().multipliedBy(0.9)
+        }
+        
+        loginTitleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(googleLoginButton.snp.top).offset(-32)
+        }
+        
+        googleLoginButton.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(16)
+            $0.bottom.equalTo(appleLoginButton.snp.top).offset(-16)
+        }
+        
+        appleLoginButton.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(safeAreaLayoutGuide).inset(16)
+            $0.centerY.equalToSuperview().multipliedBy(1.74)
+        }
+    }
+    
+    private func configureSocialLoginButton(
+        _ button: UIButton,
+        title: AttributedString,
+        image: UIImage
+    ) {
+        var config = UIButton.Configuration.filled()
+        config.image = image
+        config.imagePadding = 10
+        config.contentInsets = .init(top: 12, leading: 0, bottom: 12, trailing: 0)
+        config.attributedTitle = title
+        config.attributedTitle?.font = UIFont.pretendard(size: 19, weight: .medium)
+        config.attributedTitle?.foregroundColor = UIColor.loginText
+        config.background.strokeWidth = 1
+        config.background.strokeColor = .gray900
+        config.background.cornerRadius = 10
+        button.configuration = config
+        button.configurationUpdateHandler = { btn in
+            var config = btn.configuration
+            config?.baseBackgroundColor = btn.isHighlighted ? .gray50 : .clear
+            btn.configuration = config
+        }
+    }
 }
--- HwanYulMate/Presentation/Login/LoginViewController.swift
+++ HwanYulMate/Presentation/Login/LoginViewController.swift
@@ -7,23 +7,19 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class LoginViewController: UIViewController {
+    
+    // MARK: - properties
+    private let loginView = LoginView()
+    
+    // MARK: - life cycles
+    override func loadView() {
+        view = loginView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("viewDidLoad")
     }
-    */
-
 }
