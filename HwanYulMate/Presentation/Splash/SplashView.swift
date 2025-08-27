8d67d3db472d8e743dd127a6e2815c40b2ae2d38
Gardeniaa101 <onejwon33@naver.com>
2025-08-28 02:23:24 +0900

    [Fix] #8 - 빌드 도구 문제 해결

--- HwanYulMate/Presentation/Splash/SplashView.swift
+++ HwanYulMate/Presentation/Splash/SplashView.swift
@@ -10,7 +10,7 @@ import SnapKit
 import Then
 
 final class SplashView: BaseView {
-    
+
     // MARK: - properties
     private let splashImageView = UIImageView().then {
         $0.image = .splash
