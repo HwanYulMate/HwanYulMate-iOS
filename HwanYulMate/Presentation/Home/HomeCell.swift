a79b215044963d6174301870a1f5063a31922d75
KJH <gh7052@gmail.com>
2025-08-28 20:00:52 +0900

    [Add] #6 - arrowRight, notification 에셋 추가

---
+++ HwanYulMate/Presentation/Home/HomeCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeCell.swift
+//  HwanYulMate
+//
+//  Created by 김정호 on 8/28/25.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
