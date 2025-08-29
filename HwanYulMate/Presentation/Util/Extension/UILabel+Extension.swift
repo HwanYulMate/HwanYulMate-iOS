e798732e756ac359a7ae5a6ae05a07381354e3c1
KJH <gh7052@gmail.com>
2025-08-29 23:33:49 +0900

    [Feat] #6 - Label 텍스트의 줄 간격을 설정할 수 있는 메서드 구현

--- HwanYulMate/Presentation/Util/Extension/UILabel+Extension.swift
+++ HwanYulMate/Presentation/Util/Extension/UILabel+Extension.swift
@@ -5,4 +5,23 @@
 //  Created by 김정호 on 8/29/25.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    func setLineSpacing(spacing: CGFloat) {
+        guard let text = text else { return }
+        
+        let attributeString = NSMutableAttributedString(string: text)
+        let style = NSMutableParagraphStyle()
+        
+        style.lineSpacing = spacing
+        
+        attributeString.addAttribute(
+            .paragraphStyle,
+            value: style,
+            range: NSRange(location: 0, length: attributeString.length)
+        )
+        
+        attributedText = attributeString
+    }
+}
