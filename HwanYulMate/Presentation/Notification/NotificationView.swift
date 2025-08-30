9560f2447f3f18bf513c32245d1ce721d2b8e1f1
KJH <gh7052@gmail.com>
2025-08-30 14:49:28 +0900

    [Add] #6 - notification_on, notification_off 에셋 추가

---
+++ HwanYulMate/Presentation/Notification/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  HwanYulMate
+//
+//  Created by 김정호 on 8/30/25.
+//
+
+import UIKit
+
+class NotificationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
