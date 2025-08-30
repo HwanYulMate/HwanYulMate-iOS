1574dce89ba53a4313aefcf0b1a41889ca9085dd
KJH <gh7052@gmail.com>
2025-08-30 18:22:21 +0900

    [Add] #6 - alarm, schedule 에셋 추가

---
+++ HwanYulMate/Presentation/NotificationSetting/NotificationSettingView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationSettingView.swift
+//  HwanYulMate
+//
+//  Created by 김정호 on 8/30/25.
+//
+
+import UIKit
+
+class NotificationSettingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
