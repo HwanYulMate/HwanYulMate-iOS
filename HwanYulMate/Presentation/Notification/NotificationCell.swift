383d6311f175f41c01464e77da74abeb6ea80aed
KJH <gh7052@gmail.com>
2025-08-31 23:04:04 +0900

    [Feat] #6 - NotificationCell UI 구현

--- HwanYulMate/Presentation/Notification/NotificationCell.swift
+++ HwanYulMate/Presentation/Notification/NotificationCell.swift
@@ -6,18 +6,64 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class NotificationCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+final class NotificationCell: BaseTableViewCell {
+    
+    // MARK: - properties
+    static let identifier = "NotificationCell"
+    
+    private let countryImageView = UIImageView()
+    
+    private let currencyNameLabel = UILabel().then {
+        $0.font = .pretendard(size: 16, weight: .medium)
+        $0.textColor = .gray900
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    private let notificationButton = UIButton().then {
+        $0.setImage(.notificationOff, for: .normal)
     }
-
+    
+    // MARK: - life cycles
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        countryImageView.image = nil
+        currencyNameLabel.text = nil
+        notificationButton.setImage(.notificationOff, for: .normal)
+    }
+    
+    // MARK: - methods
+    override func configureUI() {
+        selectionStyle = .none
+    }
+    
+    override func configureHierarchy() {
+        contentView.addSubview(countryImageView)
+        contentView.addSubview(currencyNameLabel)
+        contentView.addSubview(notificationButton)
+    }
+    
+    override func configureConstraints() {
+        countryImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(16)
+            $0.width.equalTo(28)
+            $0.height.equalTo(18)
+        }
+        
+        currencyNameLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(countryImageView.snp.trailing).offset(12)
+            $0.trailing.equalTo(notificationButton.snp.leading).offset(-12)
+        }
+        
+        notificationButton.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview().inset(20)
+            $0.trailing.equalToSuperview().offset(-26)
+            $0.width.equalTo(16)
+            $0.height.equalTo(20)
+        }
+    }
 }
