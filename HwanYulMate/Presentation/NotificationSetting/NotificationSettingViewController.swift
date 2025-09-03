858c9cdcc3fae44d5692a04c4d74aa15c0ffe47d
KJH <gh7052@gmail.com>
2025-09-03 19:42:36 +0900

    [Feat] #6 - 환율 알림 설정 화면 -> 환율 알림 확인 화면으로 뒤로가기 기능 구현

--- HwanYulMate/Presentation/NotificationSetting/NotificationSettingViewController.swift
+++ HwanYulMate/Presentation/NotificationSetting/NotificationSettingViewController.swift
@@ -6,24 +6,59 @@
 //
 
 import UIKit
+import ReactorKit
+import RxCocoa
+import RxSwift
 
-class NotificationSettingViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class NotificationSettingViewController: UIViewController, View {
+    
+    // MARK: - properties
+    private let notificationSettingView = NotificationSettingView()
+    
+    var disposeBag = DisposeBag()
+    
+    // MARK: - life cycles
+    init(reactor: NotificationSettingReactor) {
+        super.init(nibName: nil, bundle: nil)
+        
+        self.reactor = reactor
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func loadView() {
+        view = notificationSettingView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+    }
+    
+    // MARK: - methods
+    private func configureUI() {
+        configureNavigation()
+    }
+    
+    private func configureNavigation() {
+        navigationItem.titleView = notificationSettingView.navigationTitleLabel
+        navigationItem.leftBarButtonItem = notificationSettingView.backBarButtonItem
+    }
+    
+    func bind(reactor: NotificationSettingReactor) {
+        notificationSettingView.backBarButtonItem.rx.tap
+            .map { NotificationSettingReactor.Action.tapBackBarButtonItem }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        reactor.action
+            .filter { $0 == .tapBackBarButtonItem }
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.popViewController(animated: true)
+            }
+            .disposed(by: disposeBag)
+    }
 }
