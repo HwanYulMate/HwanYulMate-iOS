3ad756caa21edbdd21f0397d09dab99c2d164de3
KJH <gh7052@gmail.com>
2025-09-08 04:36:09 +0900

    [Add] #13 - time_selection_selected, time_selection_unselected 이미지 에셋 추가

---
+++ HwanYulMate/Presentation/NotificationSetting/BottomSheet/TimeSelection/TimeSelectionBottomSheetCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimeSelectionBottomSheetCell.swift
+//  HwanYulMate
+//
+//  Created by 김정호 on 9/8/25.
+//
+
+import UIKit
+
+class TimeSelectionBottomSheetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
