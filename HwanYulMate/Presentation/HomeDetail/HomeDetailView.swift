58eca215c00a8289728e6582d7f0a49b0a9b6ee1
KJH <gh7052@gmail.com>
2025-09-09 22:20:09 +0900

    [Add] #14 - home_detail_news 이미지 에셋 추가

---
+++ HwanYulMate/Presentation/HomeDetail/HomeDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeDetailView.swift
+//  HwanYulMate
+//
+//  Created by 김정호 on 9/9/25.
+//
+
+import UIKit
+
+class HomeDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
