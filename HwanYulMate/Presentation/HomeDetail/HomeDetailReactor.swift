75a9ebf3596b341572610b3e1c426effd7cb3a0b
KJH <gh7052@gmail.com>
2025-09-11 20:40:20 +0900

    [Feat] #14 - 환율 정보 상세 화면 -> 환율 정보 조회 화면으로 pop 기능 구현

--- HwanYulMate/Presentation/HomeDetail/HomeDetailReactor.swift
+++ HwanYulMate/Presentation/HomeDetail/HomeDetailReactor.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import ReactorKit
+
+final class HomeDetailReactor: Reactor {
+    
+    // MARK: - nested types
+    enum Action {
+        case tapBackBarButtonItem
+    }
+    
+    enum Mutation {
+        case setRoute(Route?)
+    }
+    
+    struct State {
+        var route: Route?
+    }
+    
+    enum Route {
+        case pop
+    }
+    
+    // MARK: - properties
+    let initialState = State()
+    
+    // MARK: - methods
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .tapBackBarButtonItem:
+            return .just(.setRoute(.pop))
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        
+        switch mutation {
+        case .setRoute(let route):
+            newState.route = route
+        }
+        
+        return newState
+    }
+}
--- HwanYulMate/Presentation/HomeDetail/HomeDetailViewController.swift
+++ HwanYulMate/Presentation/HomeDetail/HomeDetailViewController.swift
@@ -6,24 +6,62 @@
 //
 
 import UIKit
+import ReactorKit
+import RxCocoa
+import RxSwift
 
-class HomeDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class HomeDetailViewController: UIViewController, View {
+    
+    // MARK: - properties
+    private let homeDetailView = HomeDetailView()
+    
+    var disposeBag = DisposeBag()
+    
+    // MARK: - life cycles
+    override func loadView() {
+        view = homeDetailView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
     }
-    */
-
+    
+    // MARK: - methods
+    private func configureUI() {
+        configureTableView()
+        configureNavigation()
+    }
+    
+    private func configureTableView() {
+        homeDetailView.tableView.register(
+            HomeDetailCell.self,
+            forCellReuseIdentifier: HomeDetailCell.identifier
+        )
+    }
+    
+    private func configureNavigation() {
+        navigationItem.titleView = homeDetailView.navigationTitleLabel
+        navigationItem.leftBarButtonItem = homeDetailView.backBarButtonItem
+    }
+    
+    func bind(reactor: HomeDetailReactor) {
+        homeDetailView.backBarButtonItem.rx.tap
+            .map { HomeDetailReactor.Action.tapBackBarButtonItem }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.route }
+            .bind(with: self) { owner, route in
+                guard let route else { return }
+                
+                switch route {
+                case .pop:
+                    owner.navigationController?.popViewController(animated: true)
+                }
+            }
+            .disposed(by: disposeBag)
+    }
 }
