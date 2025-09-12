b00696ad2e2ffe68f623b51012ad59061ca3523d
KJH <gh7052@gmail.com>
2025-09-13 04:10:46 +0900

    [Feat] #17 - NetworkService 구현

--- HwanYulMate/Infrastructure/Network/NetworkService.swift
+++ HwanYulMate/Infrastructure/Network/NetworkService.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Alamofire
+import RxSwift
+
+final class NetworkService {
+    
+    // MARK: - methods
+    func request<T: Endpoint>(_ endpoint: T) -> Single<Data> {
+        return Single.create { single in
+            do {
+                let urlRequest = try endpoint.asURLRequest()
+                let request = AF.request(urlRequest).validate(statusCode: 200..<300)
+                
+                request.responseData { response in
+                    switch response.result {
+                    case .success(let data):
+                        single(.success(data))
+                    case .failure(let error):
+                        single(.failure(error))
+                    }
+                }
+                
+                return Disposables.create { request.cancel() }
+            } catch {
+                return Disposables.create { single(.failure(error)) }
+            }
+        }
+    }
+}
