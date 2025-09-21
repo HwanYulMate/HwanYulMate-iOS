d32efd14b76487557e0f0f64056372ade1be7a15
KJH <gh7052@gmail.com>
2025-09-22 08:12:59 +0900

    [Feat] #30 - AuthEndpoint(sendFCMToken 메소드 구현), TargetPriceEndpoint, DailyAlertEndpoint 구현

--- HwanYulMate/Data/Network/DailyAlertEndpoint.swift
+++ HwanYulMate/Data/Network/DailyAlertEndpoint.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+
+enum DailyAlertEndpoint: Endpoint {
+    case enableDailyAlert(token: String, currencyCode: String, request: DailyAlertRequestDTO)
+    case disableDailyAlert(token: String, currencyCode: String)
+}
+
+// MARK: - extensions
+extension DailyAlertEndpoint {
+    var baseURL: URL {
+        URL(string: AppConfig.shared.baseURL)!
+    }
+    
+    var headers: [String: String]? {
+        switch self {
+        case .enableDailyAlert(let token, _, _):
+            return [
+                "Content-Type": "application/json",
+                "Authorization": "Bearer \(token)"
+            ]
+        case .disableDailyAlert(let token, _):
+            return [
+                "Content-Type": "application/json",
+                "Authorization": "Bearer \(token)"
+            ]
+        }
+    }
+    
+    var method: HTTPMethodType {
+        switch self {
+        case .enableDailyAlert:
+            return .post
+        case .disableDailyAlert:
+            return .delete
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .enableDailyAlert(_, let currencyCode, _):
+            return "/api/alert/setting/\(currencyCode)/daily"
+        case .disableDailyAlert(_, let currencyCode):
+            return "/api/alert/setting/\(currencyCode)/daily"
+        }
+    }
+    
+    var queryParametersEncodable: (any Encodable)? {
+        nil
+    }
+    
+    var bodyParametersEncodable: (any Encodable)? {
+        switch self {
+        case .enableDailyAlert(_, _, let request):
+            return request
+        case .disableDailyAlert:
+            return nil
+        }
+    }
+    
+    var bodyEncoder: any BodyEncoder {
+        JSONBodyEncoder()
+    }
+    
+    var responseDecoder: any ResponseDecoder {
+        JSONResponseDecoder()
+    }
+}
--- HwanYulMate/Data/Network/TargetPriceEndpoint.swift
+++ HwanYulMate/Data/Network/TargetPriceEndpoint.swift
@@ -6,3 +6,69 @@
 //
 
 import Foundation
+
+enum TargetPriceEndpoint: Endpoint {
+    case enableTargetPriceAlert(token: String, currencyCode: String, request: TargetPriceRequestDTO)
+    case disableTargetPriceAlert(token: String, currencyCode: String)
+}
+
+// MARK: - extensions
+extension TargetPriceEndpoint {
+    var baseURL: URL {
+        URL(string: AppConfig.shared.baseURL)!
+    }
+    
+    var headers: [String: String]? {
+        switch self {
+        case .enableTargetPriceAlert(let token, _, _):
+            return [
+                "Content-Type": "application/json",
+                "Authorization": "Bearer \(token)"
+            ]
+        case .disableTargetPriceAlert(let token, _):
+            return [
+                "Content-Type": "application/json",
+                "Authorization": "Bearer \(token)"
+            ]
+        }
+    }
+    
+    var method: HTTPMethodType {
+        switch self {
+        case .enableTargetPriceAlert:
+            return .post
+        case .disableTargetPriceAlert:
+            return .delete
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .enableTargetPriceAlert(_, let currencyCode, _):
+            return "/api/alert/setting/\(currencyCode)/target"
+        case .disableTargetPriceAlert(_, let currencyCode):
+            return "/api/alert/setting/\(currencyCode)/target"
+        }
+    }
+    
+    var queryParametersEncodable: (any Encodable)? {
+        nil
+    }
+    
+    var bodyParametersEncodable: (any Encodable)? {
+        switch self {
+        case .enableTargetPriceAlert(_, _, let request):
+            return request
+        case .disableTargetPriceAlert:
+            return nil
+        }
+    }
+    
+    var bodyEncoder: any BodyEncoder {
+        JSONBodyEncoder()
+    }
+    
+    var responseDecoder: any ResponseDecoder {
+        JSONResponseDecoder()
+    }
+}
